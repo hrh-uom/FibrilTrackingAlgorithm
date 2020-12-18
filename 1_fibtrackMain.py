0736bead2574bcb384a6a745c25476a9714b58e8
Helena Raymond-Hayling <helena.raymond-hayling@manchester.ac.uk>
2020-12-18 16:32:06 +0000

    first commit, abc heatmap code pasted in

--- 1_fibtrackMain.py
+++ 1_fibtrackMain.py
@@ -148,6 +148,9 @@ def lastplane_tomap(junk):
     while np.any(junk==pID):
      pID-=1
     return pID-increments_back_forward(pID, junk)[0]
+
+
+
 def M4_fibril_mapping(a,b,c, p,pp, skip=1):
     global nfibs
     global fib_rec
@@ -215,5 +218,65 @@ pp=lastplane_tomap(junk)
 a,b,c=1,1,1
 M4_fibril_mapping(a, b, c,p, pp)
 
+#%%
+def ndropped(a, b, c):
 
-#md.animation_inline(morphComp, np.arange(nfibs), fib_rec, startplane=p, endplane=pp+1)
+    fib_rec=np.full((nfibs,nplanes),-1, dtype=int); #-1 means no fibril here, as indices are >= 0
+    fib_rec[:,0]=np.arange(nfibs); #use like fib_rec[fID, pID]
+    pID=0; #starting at the top  #LOOP OVER PLANES HERE
+    dz_b, dz_f=increments_back_forward(pID,junk)
+    err_table=np.zeros([nfibs,np.max(morphComp[pID+dz_f])]); #table of errors i,j. Overwritten at each new pID
+
+
+    for fID in range(nfibs):
+        #Isolating the relevant 'patch in morphological components
+        cofI=props[pID,fib_rec[fID,pID],0:2]#centroid of fibril in plane
+        index=np.ndarray.flatten(md.search_window(cofI, npix/10, npix)).astype('int')
+        compare_me=np.delete(np.unique(np.ndarray.flatten(morphComp[pID+dz_f,index[0]:index[1], index[2]:index[3]]-1) ),0);#find a more neat way to do this. List of indices in next slice to look at.
+        #CREATING ERROR TABLES
+        for j in compare_me: #going through relevant segments in next slice
+            if fib_rec[fID,pID]!=-1: # catching nonexistent fibrils, true in pID>0
+                err_table[fID,j]=err(pID, fib_rec[fID,pID], fib_rec[fID,pID-dz_b], j,dz_b, dz_f, a, b, c);
+            else:
+                err_table[fID,j]=0.;
+
+    #sorted lists of the errors and the pairs of i,j which yield these Errors
+    sort_errs=sort_errs=np.sort(err_table, axis=None)
+    sort_err_pairs =np.transpose(np.unravel_index(np.argsort(err_table, axis=None), err_table.shape));
+
+    #delete pairs with 0 errors (ie those who are outside the box) and those above the threshold
+    delete_errs=np.concatenate((np.where(sort_errs==0)[0],np.where(sort_errs>errorthresh(a,b,c))[0]), axis=0)
+    sort_err_pairs=np.delete(sort_err_pairs,delete_errs, axis=0)
+
+
+    i=0; #Matching up
+    while sort_err_pairs.shape[0]>0:
+        match=sort_err_pairs[0]; # picks out smallest error match
+        fib_rec[match[0], pID+dz_f]=match[1]; # fills in the corresponding fibril recor with this match
+        #delete all other occurences of i,j
+        deleteme=np.unique(np.ndarray.flatten(np.concatenate((np.argwhere(sort_err_pairs[:,0]==match[0]),np.argwhere(sort_err_pairs[:,1]==match[1]))))).tolist()
+        sort_err_pairs=np.delete(sort_err_pairs, deleteme,axis=0)
+        i=i+1;
+
+    return (nfibs/np.count_nonzero(fib_rec[:,1]<0));
+
+#%%
+start_time =time_s();
+a=np.logspace(-2,2,20)
+b=np.logspace(-2,2,20)
+c=1;
+# filling the heatmap, value by value
+fun_map = np.empty((a.size, b.size))
+for i in range(a.size):
+    for j in range(b.size):
+        fun_map[i,j] = ndropped(a[i], b[j],c)
+
+md.beep();
+#%%
+fig = plt.figure()
+s = fig.add_subplot(1, 1, 1, xlabel='a', ylabel='b')
+extent = [np.log10( a[0]), np.log10( a[-1]), np.log10( b[0]), np.log10( b[-1])];
+im = plt.imshow(fun_map, extent=extent,origin='lower')
+fig.colorbar(im);
+plt.title('1 in how many fibrils dropped. Axes are powers of 10, c=1')
+plt.show()
