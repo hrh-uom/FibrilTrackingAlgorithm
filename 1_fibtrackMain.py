4c12b43e82a1d11cd4966e08a222ca0f6892eafa
Helena Raymond-Hayling <helena.raymond-hayling@manchester.ac.uk>
2020-12-22 18:45:32 +0000

    endsection plotting abc heatmap and looping over abc in mapping and printing results

--- 1_fibtrackMain.py
+++ 1_fibtrackMain.py
@@ -111,19 +111,14 @@ def err_c(pID, i,prev_i, j, dz_b, dz_f):
         prevcent=props[pID-dz_b, prev_i, 0:2]
         predictedcent=currentcent+dz_f*(currentcent-prevcent)
         return np.linalg.norm(predictedcent-props[pID+dz_f, j, 0:2])/Lscale
-
 def err_a(pID, i, j, dz_f): #error in area
  return np.abs(props[pID+dz_f, j, 3]-props[pID, i, 3])/Ascale
-
 def err_f(pID, i, j, dz_f): #error in feret diameter
  return np.abs(props[pID+dz_f, j, 5]-props[pID, i, 5])/Lscale
-
 def err(pID, fID, prev_i, j,dz_b, dz_f, a, b, c):  #not ensuring values need to ve <1
     return (1/(a+b+c)) *(a* err_c(pID, fID, prev_i,j,dz_b, dz_f)+b*err_f(pID, fID, j,dz_f)+c*err_a(pID, fID, j,dz_f))
-
 def errorthresh(a, b, c, skip): #max values for error cutoff.
  return (1/(a+b+c))*(a *skip + b + c )
-
 #--------Junk Slice Functions
 def increments_back_forward(pID, junk):
     """
@@ -149,20 +144,18 @@ def lastplane_tomap(junk):
      pID-=1
     return pID-increments_back_forward(pID, junk)[0]
 
-
-
-def M4_fibril_mapping(a,b,c, p,pp, skip=1):
+def M4_fibril_mapping(a,b,c,skip=1, fib_rec_filename="fib_rec"):
     global nfibs
     global fib_rec
     global nplanes
     start_time=time_s()
     with open(dirResults+r'\fibtrack_status_update.csv', 'a') as status_update:
-        status_update.write('\ntime '+md.t_d_stamp()+'\nJunk slices,'+str(junk)+"\npID,nfibs,time since mapping began")
+        status_update.write("\n\n a %.2f, b %.2f, c %.2f"%(a, b, c)+'\ntime '+md.t_d_stamp()+'\nJunk slices,'+str(junk)+"\npID,nfibs,time since mapping began")
     nfibs=np.max(morphComp[0]) #number eqivalent to n objects in first slice
     fib_rec=np.full((nfibs,nplanes),-1, dtype=int)  #-1 means no fibril here, as indices are >= 0
     fib_rec[:,0]=np.arange(nfibs)  #use like fib_rec[fID, pID]
 
-    for pID in range (p, pp):#(lastplane_tomap(junk)):
+    for pID in range(lastplane_tomap(junk)): #(p, pp):#
         if np.any(junk==pID):#If the slice is junk, skip the mapping.
             #x=1
             continue
@@ -208,75 +201,98 @@ def M4_fibril_mapping(a,b,c, p,pp, skip=1):
         # save/export stuff
         with open(dirResults+r'\fibtrack_status_update.csv', 'a') as status_update:
             status_update.write('\n'+','.join(map(str,[pID,nfibs,time_s()-start_time])))
-        np.save(dirResults+'fib_rec', fib_rec)
+        np.save(dirResults+fib_rec_filename, fib_rec)
 
-    print("N Tracked as a percentage: %.3f"%(np.count_nonzero(fib_rec[:, pp]>-1)/np.count_nonzero(fib_rec[:, p]>-1)))
-
-p=0
-pp=lastplane_tomap(junk)
-
-a,b,c=1,1,1
-M4_fibril_mapping(a, b, c,p, pp)
+    #print("N Tracked as a percentage: %.3f"%(np.count_nonzero(fib_rec>-1)/np.count_nonzero(fib_rec[:, p]>-1)))
 
 #%%
-def ndropped(a, b, c):
+def ndropped(a, b, c, pID_list):
 
-    fib_rec=np.full((nfibs,nplanes),-1, dtype=int); #-1 means no fibril here, as indices are >= 0
-    fib_rec[:,0]=np.arange(nfibs); #use like fib_rec[fID, pID]
-    pID=0; #starting at the top  #LOOP OVER PLANES HERE
-    dz_b, dz_f=increments_back_forward(pID,junk)
-    err_table=np.zeros([nfibs,np.max(morphComp[pID+dz_f])]); #table of errors i,j. Overwritten at each new pID
+    """
+    figuring out how many dropped in a pair of planes
+    """
+    lis=[];
+    for pID in pID_list:
+        nfibs=np.max(morphComp[pID]);
+        fib_rec=np.full((nfibs,nplanes),-1, dtype=int) #-1 means no fibril here, as indices are >= 0
+        fib_rec[:,pID]=np.arange(nfibs); #use like fib_rec[fID, pID]
+        dz_b, dz_f=increments_back_forward(pID,junk)
+        err_table=np.zeros([nfibs,np.max(morphComp[pID+dz_f])]); #table of errors i,j. Overwritten at each new pID
 
-
-    for fID in range(nfibs):
-        #Isolating the relevant 'patch in morphological components
-        cofI=props[pID,fib_rec[fID,pID],0:2]#centroid of fibril in plane
-        index=np.ndarray.flatten(md.search_window(cofI, npix/10, npix)).astype('int')
-        compare_me=np.delete(np.unique(np.ndarray.flatten(morphComp[pID+dz_f,index[0]:index[1], index[2]:index[3]]-1) ),0);#find a more neat way to do this. List of indices in next slice to look at.
         #CREATING ERROR TABLES
-        for j in compare_me: #going through relevant segments in next slice
+        for fID in range(nfibs):
+            #Isolating the relevant 'patch in morphological components
             if fib_rec[fID,pID]!=-1: # catching nonexistent fibrils, true in pID>0
-                err_table[fID,j]=err(pID, fib_rec[fID,pID], fib_rec[fID,pID-dz_b], j,dz_b, dz_f, a, b, c);
-            else:
-                err_table[fID,j]=0.;
+                cofI=props[pID,fib_rec[fID,pID],0:2]#centroid of fibril in plane
+                index=np.ndarray.flatten(md.search_window(cofI, npix/10, npix)).astype('int')
+                compare_me=np.delete(np.unique(np.ndarray.flatten(morphComp[pID+dz_f,index[0]:index[1], index[2]:index[3]]-1) ),0) #find a more neat way to do this. List of indices in next slice to look at.
+                for j in compare_me: #going through relevant segments in next slice
+                    err_table[fID,j]=err(pID, fib_rec[fID,pID], fib_rec[fID,pID-dz_b], j,dz_b, dz_f, a, b, c)
 
-    #sorted lists of the errors and the pairs of i,j which yield these Errors
-    sort_errs=sort_errs=np.sort(err_table, axis=None)
-    sort_err_pairs =np.transpose(np.unravel_index(np.argsort(err_table, axis=None), err_table.shape));
+        #sorted lists of the errors and the pairs of i,j which yield these Errors
+        sort_errs=sort_errs=np.sort(err_table, axis=None)
+        sort_err_pairs =np.transpose(np.unravel_index(np.argsort(err_table, axis=None), err_table.shape))
 
-    #delete pairs with 0 errors (ie those who are outside the box) and those above the threshold
-    delete_errs=np.concatenate((np.where(sort_errs==0)[0],np.where(sort_errs>errorthresh(a,b,c))[0]), axis=0)
-    sort_err_pairs=np.delete(sort_err_pairs,delete_errs, axis=0)
+        #delete pairs with 0 errors (ie those who are outside the box) and those above the threshold
+        delete=np.concatenate((np.where(sort_errs==0)[0],np.where(sort_errs>errorthresh(a,b,c, skip))[0]), axis=0)
+        sort_err_pairs=np.delete(sort_err_pairs,delete, axis=0)
 
+        i=0  #Matching up
+        while sort_err_pairs.shape[0]>0:
+            match=sort_err_pairs[0]  # picks out smallest error match
+            fib_rec[match[0], pID+dz_f]=match[1]  # fills in the corresponding fibril recor with this match
+            #delete all other occurences of i,j
+            deleteme=np.unique(np.ndarray.flatten(np.concatenate((np.argwhere(sort_err_pairs[:,0]==match[0]),np.argwhere(sort_err_pairs[:,1]==match[1]))))).tolist()
+            sort_err_pairs=np.delete(sort_err_pairs, deleteme,axis=0)
+            i=i+1
+        lis.append(nfibs/np.count_nonzero(fib_rec[:,pID+dz_f]<0))
+    return lis
 
-    i=0; #Matching up
-    while sort_err_pairs.shape[0]>0:
-        match=sort_err_pairs[0]; # picks out smallest error match
-        fib_rec[match[0], pID+dz_f]=match[1]; # fills in the corresponding fibril recor with this match
-        #delete all other occurences of i,j
-        deleteme=np.unique(np.ndarray.flatten(np.concatenate((np.argwhere(sort_err_pairs[:,0]==match[0]),np.argwhere(sort_err_pairs[:,1]==match[1]))))).tolist()
-        sort_err_pairs=np.delete(sort_err_pairs, deleteme,axis=0)
-        i=i+1;
-
-    return (nfibs/np.count_nonzero(fib_rec[:,1]<0));
 
 #%%
 start_time =time_s();
-a=np.logspace(-2,2,20)
-b=np.logspace(-2,2,20)
-c=1;
-# filling the heatmap, value by value
-fun_map = np.empty((a.size, b.size))
-for i in range(a.size):
-    for j in range(b.size):
-        fun_map[i,j] = ndropped(a[i], b[j],c)
 
-md.beep();
+a=1;
+b=np.logspace(-2,2,20)
+c=np.logspace(-2,2,20)
+# filling the heatmap, value by value
+fun_map = np.empty((b.size, c.size))
+def make_abc_map(fun_map):
+    for i in range(b.size):
+        print(i)
+        for j in range(c.size):
+            random_planes=np.random.choice(np.setdiff1d(np.arange(nplanes-1),junk),10)
+            fun_map[i,j] = np.mean(ndropped(a, b[i],c[j], random_planes))
+
+
 #%%
+dirResults=r'C:\Users\t97721hr\Dropbox (The University of Manchester)\Fibril Tracking Algorithm\FTA_for_publication\abc_experiment_december'
+
+fun_map=np.load(dirResults+"\\heatmap_abc_dec.npy")
+
 fig = plt.figure()
-s = fig.add_subplot(1, 1, 1, xlabel='a', ylabel='b')
-extent = [np.log10( a[0]), np.log10( a[-1]), np.log10( b[0]), np.log10( b[-1])];
-im = plt.imshow(fun_map, extent=extent,origin='lower')
+s = fig.add_subplot(1, 1, 1, xlabel='b', ylabel='c')
+extent = [np.log10( b[0]), np.log10( b[-1]), np.log10( c[0]), np.log10( c[-1])];
+im = plt.imshow(fun_map.T,extent=extent, origin='lower') #the transpose is because of the row column effect
 fig.colorbar(im);
-plt.title('1 in how many fibrils dropped. Axes are powers of 10, c=1')
-plt.show()
+plt.title('1 in how many fibrils dropped. Axes are powers of 10, a=1')
+#plt.savefig('abc_investigation_dec.png')
+#print ((time_s()-start_time)/60)
+#np.save("heatmap_abc_dec", fun_map)
+
+#%%
+#SORTING THE VALUES OF B AND C
+sort_pairs=np.vstack(np.unravel_index((-fun_map).argsort(axis=None, kind='mergesort'), fun_map.shape))
+b_c_values_sorted=np.vstack((b[sort_pairs[0]],c[sort_pairs[1]])).T
+
+
+##%%
+
+for i in range(10):
+    a=1
+    b,c=b_c_values_sorted[i]
+    abc_string="_rank_%d_a_%.2f_b_%.2f_c_%.2f"%(i+1, a, b, c)
+    M4_fibril_mapping(a, b, c, fib_rec_filename='\\fibrec'+abc_string)
+    #print('completed %d of 10 rounds of b,c '%i+1)
+
+md.beep()
