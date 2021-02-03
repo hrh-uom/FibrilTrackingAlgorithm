9229462f8f14bb589e27e8c61b5c8559f6844f79
Helena Raymond-Hayling <helena.raymond-hayling@manchester.ac.uk>
2021-02-03 17:17:16 +0000

    biscuit is working. Tweak error thresh next

--- 1_fibtrackMain.py
+++ 1_fibtrackMain.py
@@ -45,23 +45,26 @@ def compress_by_skipping(skip):
         nplanes=imgstack.shape[0]
         dirResults= dir3V+'skip_%d_results\\'%skip
 def create_morph_comp(imgstack):
-    morphComp=np.zeros([nplanes, npix, npix], dtype=int) #initialising array for morph comp
+    """
+    a 3d Labelled array of image stack. Named for the morphological components function in mathematica. In each plane, every object gets a unique label, labelled from 1 upwards. The background is labelled 0.
+    """
+    MC=np.zeros([nplanes, npix, npix], dtype=int) #initialising array for morph comp
     for i in range(nplanes):
-        morphComp[i]=label(imgstack[i])
-    np.save(dirResults+'morphComp', morphComp)
-    return morphComp
-def create_properties_table(morphComp):
+        MC[i]=label(imgstack[i])
+    np.save(dirResults+'morphComp', MC)
+    return MC
+def create_properties_table(MC):
     """
     Setting up table of properties for each plane (props) props stores (pID, objectID, property). it is the length of the max number of objects in any plane, and populated with zeroes.
     """
     props_ofI='centroid','orientation','area','eccentricity' # these properties are the ones to be calculated using skimage.measure
-    props=np.empty((nplanes, np.max(morphComp), len(props_ofI)+2)) #the +2 is because centroid splits into 2, and also to leave space for the feret diameter, calculated by a custom script.
+    props=np.empty((nplanes, np.max(MC), len(props_ofI)+2)) #the +2 is because centroid splits into 2, and also to leave space for the feret diameter, calculated by a custom script.
     for pID in range(nplanes):
-        rprops=pd.DataFrame(regionprops_table(morphComp[pID], properties=props_ofI)).to_numpy() #regionprops from skimage.measure
+        rprops=pd.DataFrame(regionprops_table(MC[pID], properties=props_ofI)).to_numpy() #regionprops from skimage.measure
         #print (temp.shape)
         nobj=rprops.shape[0]; # nobjects in plane
         props[pID,0:nobj, 0:5]=rprops
-        props[pID,0:nobj, 5]=feret_diameters_2d(morphComp[pID])
+        props[pID,0:nobj, 5]=feret_diameters_2d(MC[pID])
     np.save(dirResults+'props', props)
     return props
     #return temp.shape
@@ -77,12 +80,12 @@ if fromscratch:
     nplanes, npix, _ = imgstack.shape #measure array dims
     if skip>1:
         compress_by_skipping(skip)
-    morphComp=create_morph_comp(imgstack);
-    props=create_properties_table(morphComp)
+    MC=create_morph_comp(imgstack);
+    props=create_properties_table(MC)
 else: #to save time
-    morphComp=np.load(dirResults+"morphComp.npy")
+    MC=np.load(dirResults+"morphComp.npy")
     props=np.load(dirResults+"props.npy")
-    nplanes, npix, _=morphComp.shape
+    nplanes, npix, _=MC.shape
 
 Lscale=np.median(np.ravel(props[:,:,5])) # A typical lengthscale, calculated from the median value of feret diameter, in pixels. To find the equiv in nm, multiply by pxsize
 
@@ -108,8 +111,8 @@ def err_f(pID, i, j, dz_f): #error in feret diameter
  return np.abs(props[pID+dz_f, j, 5]-props[pID, i, 5])/Lscale
 def err(pID, fID, prev_i, j,dz_b, dz_f, a, b, c):  #not ensuring values need to ve <1
     return (1/(a+b+c)) *(a* err_c(pID, fID, prev_i,j,dz_b, dz_f)+b*err_f(pID, fID, j,dz_f)+c*err_a(pID, fID, j,dz_f))
-def errorthresh(a, b, c, skip): #max values for error cutoff.
- return (1/(a+b+c))*(a *skip + b + c )
+def errorthresh(a, b, c, skip, reduction=0): #max values for error cutoff.
+ return (1/(a+b+c))*(a *skip + b + c )*(1+reduction)
 #--------Junk Slice Functions
 def increments_back_forward(pID, junk):
     """
@@ -135,21 +138,17 @@ def lastplane_tomap(junk):
      pID-=1
     return pID-increments_back_forward(pID, junk)[0]
 
-def initialise_fibril_record():
-    global nfibs
-    global fib_rec
-    nfibs=np.max(morphComp[0]) #number eqivalent to n objects in first slice
-    fib_rec=np.full((nfibs,nplanes),-1, dtype=int)  #-1 means no fibril here, as indices are >= 0
-    fib_rec[:,0]=np.arange(nfibs)  #use like fib_rec[fID, pID]
-    #return fib_rec
+def initialise_fibril_record(MC):
+    nfibs=len(np.unique(MC[0]))-1 #number eqivalent to n objects in first slice
+    FR_local=np.full((nfibs,nplanes),-1, dtype=int)  #-1 means no fibril here, as indices are >= 0
+    FR_local[:,0]=np.unique(MC[0])[1:]-1  #use like FR_local[fID, pID]
+    #return FR_local
+    return FR_local
 
-def fibril_mapping(a,b,c,skip=1, rAnge=lastplane_tomap(junk)):
+def fibril_mapping(a,b,c, MC, FR_local, skip=1, reduction=0, rAnge=lastplane_tomap(junk)):
     """
     Populates fibril record, from top plane through the volume
     """
-    global fib_rec
-    global nfibs
-    FR_local=fib_rec.copy()
     start_time=time_s()
     nfibs=FR_local.shape[0]
     with open(dirResults+r'\fibtrack_status_update.csv', 'a') as status_update:
@@ -160,7 +159,7 @@ def fibril_mapping(a,b,c,skip=1, rAnge=lastplane_tomap(junk)):
             #x=1
             continue
         dz_b, dz_f=increments_back_forward(pID,junk)
-        err_table=np.zeros([nfibs,np.max(morphComp[pID+dz_f])])  #table of errors i,j.Overwritten at each new pID
+        err_table=np.zeros([nfibs,np.max(MC[pID+dz_f])])  #table of errors i,j.Overwritten at each new pID
 
         #CREATING ERROR TABLES
         for fID in range(nfibs):
@@ -168,7 +167,7 @@ def fibril_mapping(a,b,c,skip=1, rAnge=lastplane_tomap(junk)):
             if FR_local[fID,pID]!=-1: # catching nonexistent fibrils, true in pID>0
                 cofI=props[pID,FR_local[fID,pID],0:2]#centroid of fibril in plane
                 index=np.ndarray.flatten(md.search_window(cofI, npix/10, npix)).astype('int')
-                compare_me=np.delete(np.unique(np.ndarray.flatten(morphComp[pID+dz_f,index[0]:index[1], index[2]:index[3]]-1) ),0) #find a more neat way to do this. List of indices in next slice to look at.
+                compare_me=np.delete(np.unique(np.ndarray.flatten(MC[pID+dz_f,index[0]:index[1], index[2]:index[3]]-1) ),0) #find a more neat way to do this. List of indices in next slice to look at.
                 for j in compare_me: #going through relevant segments in next slice
                     err_table[fID,j]=err(pID, FR_local[fID,pID], FR_local[fID,pID-dz_b], j,dz_b, dz_f, a, b, c)
 
@@ -177,7 +176,7 @@ def fibril_mapping(a,b,c,skip=1, rAnge=lastplane_tomap(junk)):
         sort_err_pairs =np.transpose(np.unravel_index(np.argsort(err_table, axis=None), err_table.shape))
 
         #delete pairs with 0 errors (ie those who are outside the box) and those above the threshold
-        delete=np.concatenate((np.where(sort_errs==0)[0],np.where(sort_errs>errorthresh(a,b,c, skip))[0]), axis=0)
+        delete=np.concatenate((np.where(sort_errs==0)[0],np.where(sort_errs>errorthresh(a,b,c, skip, reduction))[0]), axis=0)
         sort_err_pairs=np.delete(sort_err_pairs,delete, axis=0)
 
         i=0  #Matching up
@@ -190,7 +189,7 @@ def fibril_mapping(a,b,c,skip=1, rAnge=lastplane_tomap(junk)):
             i=i+1
 
         #LOOK FOR MISSED FIBRILS
-        all=np.arange(np.max(morphComp[pID+dz_f]))
+        all=np.arange(np.max(MC[pID+dz_f]))
         mapped=np.unique(FR_local[:,pID+dz_f][FR_local[:,pID+dz_f]>-1])
         new_objects=np.setdiff1d(all, mapped)
         fibrec_append=np.full((new_objects.size,nplanes),-1, dtype=int)  #an extra bit to tack on the end of the fibril record accounting for all these new objects
@@ -202,24 +201,20 @@ def fibril_mapping(a,b,c,skip=1, rAnge=lastplane_tomap(junk)):
         with open(dirResults+r'\fibtrack_status_update.csv', 'a') as status_update:
             status_update.write('\n'+','.join(map(str,[pID,nfibs,time_s()-start_time])))
         #np.save(dirResults+'fib_rec', FR_out)
-    fib_rec=FR_local.copy()
+    return FR_local
 
-def trim_fib_rec(frac=0.9):
+def trim_fib_rec(FR_local,frac=0.9):
     """
     Trims fibril record to fibrils which are less than some fraction of the total number of planes
     """
-    global fib_rec
-    global nfibs
-    FR_local=fib_rec.copy()
     #Q: How long are all the fibrils in the original fibril rec?
+    nfibs=FR_local.shape[0]
     nexist=np.zeros(nfibs, dtype='int')
     for i in range(nfibs):
         nexist[i]=np.max(np.nonzero(FR_local[i]>-1))-np.min(np.nonzero(FR_local[i]>-1))+1
     longfibs=np.where(nexist>nplanes*frac)[0]  #the indices of the long fibirls
     #Erasing fibril record for short fibrils. The only way to map between the two is using longfibs. Reindexing also!
-    fib_rec=FR_local[longfibs].copy()
-    nfibs=longfibs.size
-
+    return FR_local[longfibs]
 
 #%%---------------------------------------------------------------------------
 #.................................4. MAIN FLOW ...........................
@@ -227,42 +222,58 @@ def trim_fib_rec(frac=0.9):
 
 a,b,c=1,1,1
 
-initialise_fibril_record()
+FR_core=initialise_fibril_record(MC)
 
-fib_rec.shape
-nfibs
-
-fibril_mapping(a, b, c)
-nfibs
+FR_core.shape
 
 
-fib_rec.shape
-trim_fib_rec()
-fib_rec.shape
-nfibs
+FR_core=fibril_mapping(a, b, c, MC,FR_core)
+FR_core.shape
 
 
 
-md.animation_inline(morphComp,np.arange(nfibs), fib_rec,0,nplanes)
+FR_core=trim_fib_rec(FR_core)
+FR_core.shape[0]
 
-
-
+md.animation_inline(MC,np.arange(FR_core.shape[0]), FR_core,0,nplanes)
 
 #%%---------------------------------------------------------------------------
 #.................................SANDBOX....................................
 #-------------------------------------------------------------------------------
-def make_biscuit_morph_comp():
+def make_biscuit_morph_comp(MC_local, FR):
     """
     Makes a copy of morphological components removing all of the fibrils that are tracked. Like when you cut load of biscuits from a rolled out piece of pastry and have the leftovers.
     """
-    morphCompDynamic=morphComp.copy() #this morph comp will have elements deleted as they are mapped.
+    nfibs=FR.shape[0]
     for pID in range (nplanes):
-        all_obj=np.unique(morphCompDynamic[pID])-1;all_obj=all_obj[all_obj>-1]
-        tracked_obj=fib_rec[:,pID][fib_rec[:,pID]>-1]
-        tracked_indices=np.array(np.unravel_index(np.where(tracked_obj[:,None]+1== morphCompDynamic[pID].ravel())[1], (npix,npix))).T #https://stackoverflow.com/questions/49987552/numpy-where-used-with-list-of-values
+        all_obj=np.unique(MC_local[pID])-1;all_obj=all_obj[all_obj>-1]
+        tracked_obj=FR[:,pID][FR[:,pID]>-1]
+        tracked_indices=np.array(np.unravel_index(np.where(tracked_obj[:,None]+1== MC_local[pID].ravel())[1], (npix,npix))).T #https://stackoverflow.com/questions/49987552/numpy-where-used-with-list-of-values
         for k in range (len(tracked_indices)):
             #effectively deleting mapped fibrils, leaving us with the leftovers
-            morphCompDynamic[pID, tracked_indices[k,0], tracked_indices[k,1]]=0
-    return morphCompDynamic
-MC2=make_biscuit_morph_comp()
-md.animation_inline(MC2,np.arange(nfibs), fib_rec,0,nplanes)
+            MC_local[pID, tracked_indices[k,0], tracked_indices[k,1]]=0
+    return MC_local
+MC2=make_biscuit_morph_comp(MC, FR_core)
+
+#%%
+md.animation_inline(MC2,np.arange(1), FR_core,0,nplanes)
+
+
+##%% RUNNING ON BISCUIT leftovers
+
+FR_extra=initialise_fibril_record(MC2)
+
+FR_extra.shape
+
+
+FR_extra=fibril_mapping(a, b, c, MC2,FR_extra, reduction=5)
+FR_extra.shape
+
+
+
+FR_extra=trim_fib_rec(FR_extra)
+FR_extra.shape[0]
+
+
+md.animation_inline(MC2,np.arange(FR_extra.shape[0]), FR_extra,0,nplanes)
+md.beep()
