50a40b8782f53e2f2157fead8754028c48ce7ea9
Helena Raymond-Hayling <helenarh7@gmail.com>
2022-05-13 10:18:32 +0100

    minor updates

--- customFunctions.py
+++ customFunctions.py
@@ -14,7 +14,6 @@ import matplotlib.patches as patches
 from  datetime import datetime as dt
 import os
 from tqdm import tqdm
-from a0_initialise import *
 plt.style.use('./mystyle.mplstyle')
 
 #----------------DIRECTORIES AND PATHS-------------------
@@ -24,8 +23,6 @@ def create_Directory(directory):
     """
     if not os.path.exists(directory):
         os.makedirs(directory)
-
-
 #----------------TIMING--------------------
 def t_d_stamp():
     return "{:%y-%m-%d_%H-%M}".format(dt.now())
@@ -33,7 +30,7 @@ def t_stamp():
     return "{:%H-%M-%S}".format(dt.now())
 def beep():
     winsound.Beep(500, 800);
-def print_status(update):
+def print_status(update, d):
     with open(d.dirOutputs+f'status_update.csv', 'a') as status_update:
         status_update.write(update)
 #-------------SEARCH WINDOWS FOR FIBRIL TRACKING --------------
@@ -62,27 +59,25 @@ def viewwindow(fID,pID, cofI,size, npix, nplanes, morphComp): #View window of se
 
 #-------------TRIM FIBRIL RECORD-------------
 
-def trim_fib_rec(FR_local, MC, dirOutputs,frac, save=True):
+def trim_fib_rec(FR_0, MC, dirOutputs,frac, save=True):
     """
     Trims fibril record to fibrils which are less than some fraction of the total number of planes
     """
     print(f'trimming fibril record to {100*frac} percent')
     #Q: How long are all the fibrils in the original fibril rec?
-    nfibs, nplanes=FR_local.shape
+    nfibs, nplanes=FR_0.shape
     nexist=np.zeros(nfibs, dtype='int')
     # print(f'nfibs {nfibs, nplanes, nexist}')
-    for i in range(nfibs):
-        if i in np.arange(0, nfibs)[0::1000]:
-            print (f"Fibril {i} of {nfibs}")
-        nexist[i]=np.max(np.nonzero(FR_local[i]>-1))-np.min(np.nonzero(FR_local[i]>-1))+1
+    for i in tqdm(range(nfibs)):
+        nexist[i]=np.max(np.nonzero(FR_0[i]>-1))-np.min(np.nonzero(FR_0[i]>-1))+1
     # print(f'nexist {np.unique(nexist)}')
     longfibs=np.where(nexist>nplanes*frac)[0]  #the indices of the long fibirls
     #Erasing fibril record for short fibrils. The only way to map between the two is using longfibs. Reindexing also!
     if save:
-        np.save(dirOutputs+f'fib_rec_trim_{100*frac}', FR_local[longfibs])
-        labels=label_volume(MC, np.arange(longfibs.size),FR_local[longfibs], nplanes )
+        np.save(dirOutputs+f'fib_rec_trim_{100*frac}', FR_0[longfibs])
+        labels=label_volume(MC, np.arange(longfibs.size),FR_0[longfibs], nplanes )
         np.save(dirOutputs+f'labelled_vol_{100*frac}', labels)
-    return FR_local[longfibs]
+    return FR_0[longfibs]
 
 #------------------ANIMATION AND VISUALISATION-----------------------------#
 def label_volume(morphComp,fib_group,fib_rec,endplane, startplane=0):
@@ -101,19 +96,37 @@ def label_volume(morphComp,fib_group,fib_rec,endplane, startplane=0):
              labels[j]=np.where(morphComp[pID]==fib_rec[fib_group[i], pID]+1, value, labels[j])
         j+=1
     return labels
+#
+# def custom_RGB_maker(fib_group, labels):
+#     """
+#     Takes a 3D array of labels and makes each label a different colour in terms of RGB [0, 0, 0] to [255, 255, 255]
+#     Custom version of skimage.color.label2rgb which uses int data type to save memory
+#     """
+#     cols=np.random.randint(0, 255, (np.max(labels)+10, 3), dtype='uint8'); cols[0]=[0,0,0]; cols[1]=[255,255,255]
+#     # cols=np.full((np.max(labels)+10, 3), 255, dtype='uint8'); cols[0]=[0,0,0];
+#
+#     RGB_vol=np.zeros((labels.shape[0], labels.shape[1], labels.shape[2], 3), dtype='uint8')
+#     for i in np.arange(labels.shape[0]):
+#         RGB_plane=cols[(labels[i]+1)] #https://forum.image.sc/t/skimage-color-label2rgb-but-choose-specific-colors-for-specific-labels/62500
+#         RGB_vol[i]=RGB_plane
+#     return RGB_vol
 
 def custom_RGB_maker(fib_group, labels):
     """
     Takes a 3D array of labels and makes each label a different colour in terms of RGB [0, 0, 0] to [255, 255, 255]
     Custom version of skimage.color.label2rgb which uses int data type to save memory
     """
-    cols=np.random.randint(0, 255, (len(fib_group), 3), dtype='uint8'); cols[0]=[0,0,0]
+    # cols=np.random.randint(0, 255, (np.max(labels)+10, 3), dtype='uint8'); cols[0]=[0,0,0]; cols[1]=[255,255,255]
+    # cols=np.full((np.max(labels)+10, 3), 255, dtype='uint8'); cols[0]=[0,0,0];
+    cols=np.full(((np.max(labels)+10, 3)), 255)
+    for i in fib_group:
+        cols[i]=np.random.randint(0, 255, (1, 3), dtype='uint8')
+    cols[0]=[0,0,0];cols[1]=[255,255,255]
     RGB_vol=np.zeros((labels.shape[0], labels.shape[1], labels.shape[2], 3), dtype='uint8')
-    for i in np.arange(labels.shape[0]-1):
+    for i in np.arange(labels.shape[0]):
         RGB_plane=cols[(labels[i]+1)] #https://forum.image.sc/t/skimage-color-label2rgb-but-choose-specific-colors-for-specific-labels/62500
         RGB_vol[i]=RGB_plane
     return RGB_vol
-
 def create_animation(fib_group, labels, startplane, endplane, dt, fig_size=10, step=1,colourful=True):
     """
     Create mapping animation object
@@ -140,16 +153,16 @@ def create_animation(fib_group, labels, startplane, endplane, dt, fig_size=10, s
 def export_animation(resultsDir,fib_group, labels, title='volumerendering/stepthrough-animation', startplane=0, endplane=0, dt=500, figsize=20,step=1):
     ani=create_animation(fib_group, labels, startplane, endplane, dt, figsize , step)
     ani.save(resultsDir+title+'.mp4')
-def animation_inline(fib_group, labels, startplane=0, endplane=0, dt=500, figsize=10, step=1):
+def animation_inline(fib_group, labels, startplane=0, endplane=0, dt=500, figsize=10, step=1, fas_coords=0):
     """
     Animate a certain group of fibrils, given a specific fibril record
     """
-    ani=create_animation(fib_group, labels, startplane, endplane, dt, figsize, step)
+    ani=create_animation(fib_group, labels, startplane, endplane, dt, figsize, step, fas_coords=fas_coords)
     return HTML(ani.to_html5_video())
 
 
 #---------------PLOTS-------------------
-def my_histogram(arr,xlabel, dens=False, title=0, labels=[], binwidth=10, xlims=0, pi=False,filename=0, leg=False, fitdata=0, fitparams=0):
+def my_histogram(arr,xlabel, show, dens=False, title=0, labels=[], binwidth=10, xlims=0, pi=False,filename=0, leg=False, fitdata=0, fitparams=0, units=''):
     """
     A histogram, with number on the y axis
     """
@@ -161,7 +174,7 @@ def my_histogram(arr,xlabel, dens=False, title=0, labels=[], binwidth=10, xlims=
     ax.hist(arr, bins=bins, density=dens, histtype='bar', edgecolor='black', label=labels)
     ax.set_xlabel(xlabel)
     ax.tick_params(axis="x", bottom=True, top=True, labelbottom=True, labeltop=True)
-    ylabel='Density' if dens else 'Number'
+    ylabel='Probability density' if dens else 'Number'
     ax.set_ylabel(ylabel); ax.margins(0);
     if xlims!=0:
         ax.xlim(xlims)
@@ -173,6 +186,26 @@ def my_histogram(arr,xlabel, dens=False, title=0, labels=[], binwidth=10, xlims=
     if leg:
         ax.legend()
 
+
+    if np.any(fitparams):
+        if len(fitparams)<=2:
+            textstr=f'$\mu$ = {fitparams[1]:.3f}{units}\n$\sigma$ = {fitparams[0]:.3f}{units}'
+            # these are matplotlib.patch.Patch properties
+            props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+            # place a text box in upper left in axes coords
+            ax.text(0.8, 0.9, textstr, transform=ax.transAxes, fontsize=18,
+            verticalalignment='top', bbox=props)
+        else:
+            ax.margins(0.05, 0.05)
+            t0=f'$w={fitparams[4]:.3f}$\n'
+            t1=f'$\mu_1$ = {fitparams[1]:.0f} nm, $\sigma_1$ = {fitparams[0]:.1f} nm, $w={fitparams[4]:.2f}$\n'
+            t2=f'$\mu_2$ = {fitparams[3]:.0f} nm, $\sigma_2$ = {fitparams[2]:.1f} nm'
+            textstr=t1+t2
+            # these are matplotlib.patch.Patch properties
+            props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+            # place a text box in upper left in axes coords
+            ax.text(0.05, 0.95, textstr, transform=ax.transAxes, fontsize=14,
+            verticalalignment='top', bbox=props)
     if np.any(fitdata):
         if dens:
             ax.plot(fitdata[0], fitdata[1], 'r')
@@ -180,19 +213,13 @@ def my_histogram(arr,xlabel, dens=False, title=0, labels=[], binwidth=10, xlims=
             ax2=ax.twinx()
             ax2.plot(fitdata[0], fitdata[1], 'r')
 
-            ax2.set_ylabel('Density')
+            ax2.set_ylabel('Probability density')
             ax2.margins(0)
             ax2.grid(False)
-    if np.any(fitparams):
-        textstr=f'$\sigma$ = {fitparams[0]:.3f} \n$\mu$ = {fitparams[1]:.3f}'
-        # these are matplotlib.patch.Patch properties
-        props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
-        # place a text box in upper left in axes coords
-        ax.text(0.8, 0.9, textstr, transform=ax.transAxes, fontsize=18,
-        verticalalignment='top', bbox=props)
     if filename!=0:
         plt.savefig(filename)
-    plt.show()
+    if show:
+        plt.show()
 
 #------------------------------STATS---------------------
 
